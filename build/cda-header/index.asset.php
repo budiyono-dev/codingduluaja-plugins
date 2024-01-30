ec221b12841fc727669fb73557f386cdaf6f0838
budiyono-dev <budiyono.dev@gmail.com>
2024-01-30 18:16:54 +0700

    update build

--- build/cda-header/index.asset.php
+++ build/cda-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '431b46e4439ada669393');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ca8795ea497156b2c5e8');
